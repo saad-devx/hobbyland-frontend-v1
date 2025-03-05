44a9fd7dfbd3c5734009cec9331f039cddbe6a24
Saad <binarshadsaad6@gmail.com>
2025-03-05 14:24:31 +0500

    updated bakend API hosts

--- src/config/Axiosconfig/index.js
+++ src/config/Axiosconfig/index.js
@@ -1,5 +1,5 @@
-export const BASEURL = "http://localhost:8000/api/";
+export const BASEURL = process.env.NEXT_PUBLIC_BASEURL || "http://localhost:8000/api/";
 
-export const BASECHATURL = "http://localhost:8001";
-export const WEBRTCBASEURL = "http://localhost:8002";
+export const BASECHATURL = process.env.NEXT_PUBLIC_BASECHATURL || "http://localhost:8001";
+export const WEBRTCBASEURL = process.env.NEXT_PUBLIC_WEBRTCBASEURL || "http://localhost:8002";
 
